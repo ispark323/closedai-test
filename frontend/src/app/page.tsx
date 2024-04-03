08fd55618f60eb20101bdf39c25f6ae60bdfc663
sungmin park <spark323@gmail.com>
2024-04-04 08:50:17 +1000

    Prettier format change

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,23 +1,23 @@
-"use client";
-import * as z from "zod";
-import { useForm } from "react-hook-form";
+'use client';
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 
 const formSchema = z.object({
   city: z.string(),
-}); 
+});
 
 export default function Home() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      city: "",
-    }
-  })
-  
+      city: '',
+    },
+  });
+
   const handleSubmit = async (values: z.infer<typeof formSchema>) => {
     console.log({ values });
     try {
@@ -27,27 +27,32 @@ export default function Home() {
     } catch (error) {
       console.error('Error fetching data:', error);
     }
-  }
+  };
 
   return (
-   <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <Form {...form}>
-      <form 
-        onSubmit={form.handleSubmit(handleSubmit)}
-        className="max-w-md w-full flex flex-col gap-4"
-      >
-        <FormField control={form.control} name="city" render={({ field }) => {
-          return <FormItem>
-            <FormLabel>Weather Forecast</FormLabel>
-            <FormControl>
-              <Input placeholder='Search City'{...field} />
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        }} />
-        <Button type='submit' className='w-full'>Submit</Button>
-      </form>
-    </Form>
-   </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleSubmit)} className="max-w-md w-full flex flex-col gap-4">
+          <FormField
+            control={form.control}
+            name="city"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Weather Forecast</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Search City" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          <Button type="submit" className="w-full">
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </main>
   );
 }
